fa56fbc5f37ffb335ebb56ccd46dbdf75bed5526
manh <manhtran40kc@gmail.com>
2023-05-22 14:33:13 +0700

    curry func

--- catz/common/curring.py
+++ catz/common/curring.py
@@ -1,7 +1,16 @@
-import catz.common.functions as functions
+from functools import wraps
 
 
-def curry(func):
-    num_params = func.__code__.co_argcount
-    curry_func = getattr(functions, f'curry{num_params}')
-    return curry_func(func)
+def curry_rec(func, args):
+    @wraps(func)
+    def wrapper(*args2):
+        new_args = args + list(args2)
+        if len(new_args) >= func.__code__.co_argcount:
+            return func(*new_args)
+        else:
+            return curry_rec(func, new_args)
+
+    return wrapper
+
+
+curry = curry_rec(lambda f: curry_rec(f, []), [])
